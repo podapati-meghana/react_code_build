60999bb2addfdbe74af45ec1570fd005791b2bab
Kaliprasad Sahoo <kalsahoo@microsoft.com>
2025-03-18 17:04:25 +0530

    1st

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="helloWorldText">
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
